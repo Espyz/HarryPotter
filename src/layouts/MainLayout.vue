1900254239147ad4373715c2dd6dc83c726601cc
Espyz <truharandrey1015@gmail.com>
2024-12-04 21:09:14 +0500

    Собрал Layout, настроил редирект, приступаю к главной странице

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,102 +1,58 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+    <div class="layout-container">
+        <div class="row full-width">
+            <div class="col-6 layoutLogo">HP</div>
+            <q-tabs
+                :model-value="route.name"
+                class="col-6"
+                align="right"
+                active-class="layoutTabs-active"
+                @update:model-value="redirect"
+            >
+                <q-tab
+                    v-for="(page, ind) in pages"
+                    :key="ind"
+                    v-bind="page"
+                    class="q-ma-md layoutTabs"
+                >
+                </q-tab>
+            </q-tabs>
+        </div>
+    </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+<script>
+import {
+    useRoute,
+    useRouter,
+} from 'vue-router';
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
+export default {
+    data() {
+        return {
+            pages:  [
+                {
+                    name:  'info',
+                    label: 'О фильме',
+                },
+                {
+                    name:  'actors',
+                    label: 'Актёры',
+                },
+                {
+                    name:  'music',
+                    label: 'Музыка',
+                },
+            ],
+            route:  useRoute(),
+            router: useRouter(),
+        };
+    },
+    
+    methods: {
+        redirect(name) {
+            this.router.push('/' + name);
+        },
+    },
+};
 </script>
